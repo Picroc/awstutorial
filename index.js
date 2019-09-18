37b68d6b25156a4abc823e4add717e7763a3a377
Aleksey Logachev <alekseylogachov@Alekseys-MacBook-Pro.local>
2019-09-18 21:10:12 +0300

    Finally

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello Papa!'))
+var redis = require('redis')
+var client = redis.createClient()
+
+client.on('error', function (err) {
+  console.log('Error ' + err)
+})
+
+app.get('/', (req, res) => {
+    client.get('usercounter', (err, rep) => {
+        client.set('usercounter', rep + 1, (err2, resp) => {
+            res.send("Current usercounter is " + resp);
+        });
+    });
+    
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require('redis')
+var client = redis.createClient({host: 'redis'})
+
+client.on('error', function (err) {
+  console.log('Error ' + err)
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.incr('usercounter', (err, rep) => {
+    client.get('usercounter', (err2, resp) => {
+      res.render('index', { title: 'Hello! ' + resp + ' users visited this site!' });
+    });
+  });
 });
 
 module.exports = router;
